32fea0ef5b8309a9df4b6009cc8decb63fe24852
fasteque <info@fasteque.com>
2015-05-24 17:05:11 +0200

    Added main RecyclerView

--- app/src/main/java/com/fasteque/playground/activities/MainActivity.java
+++ app/src/main/java/com/fasteque/playground/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.fasteque.playground.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,8 +14,11 @@ import butterknife.InjectView;
 
 
 public class MainActivity extends AppCompatActivity {
-    @InjectView(R.id.toolbar)
+    @InjectView(R.id.shows_toolbar)
     Toolbar toolbar;
+
+    @InjectView(R.id.shows_recyclerView)
+    RecyclerView showsRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
