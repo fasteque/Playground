cca7217139eb5836cb530115ce54b9a94911617b
fasteque <info@fasteque.com>
2015-05-25 11:57:20 +0200

    Better code comments

--- app/src/main/java/com/fasteque/playground/injection/components/ApplicationComponent.java
+++ app/src/main/java/com/fasteque/playground/injection/components/ApplicationComponent.java
@@ -11,8 +11,18 @@ import dagger.Component;
  * Created by danielealtomare on 24/05/15.
  * Project: Playground
  */
-@Singleton
+
+/**
+ * Components are interfaces (or abstract classes) defined by the developer.
+ * Such interface may contain two types of methods:
+ * - methods which return object and have no parameters
+ *      > instances created by Dagger2
+ * - methods which return void but have one argument
+ *      > allow injecting non-private field members to provided object as argument
+ */
+@Singleton  // Constraints this component to one-per-application or un-scoped bindings.
 @Component(modules = ApplicationModule.class)
 public interface ApplicationComponent {
+    // Exposed to sub-graphs.
     PlaygroundApplication application();
 }
--- app/src/main/java/com/fasteque/playground/injection/modules/ApplicationModule.java
+++ app/src/main/java/com/fasteque/playground/injection/modules/ApplicationModule.java
@@ -11,6 +11,11 @@ import dagger.Provides;
  * Created by danielealtomare on 24/05/15.
  * Project: Playground
  */
+
+/**
+ * It defines provider methods for any injectable dependency.
+ * It encapsulates knowledge of how to construct the objects to inject.
+ */
 @Module
 public class ApplicationModule {
     private final PlaygroundApplication playgroundApplication;
@@ -19,6 +24,7 @@ public class ApplicationModule {
         this.playgroundApplication = playgroundApplication;
     }
 
+    // Provider method: returned type is the type which you want to inject.
     @Provides
     @Singleton
     PlaygroundApplication provideApplicationContext() {
