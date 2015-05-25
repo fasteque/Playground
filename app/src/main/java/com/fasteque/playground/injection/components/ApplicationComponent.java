126fc484938a2173d8561a8f178f10fe285d1041
fasteque <info@fasteque.com>
2015-05-25 17:16:34 +0200

    Improved comments

--- app/src/main/java/com/fasteque/playground/injection/components/ApplicationComponent.java
+++ app/src/main/java/com/fasteque/playground/injection/components/ApplicationComponent.java
@@ -23,6 +23,9 @@ import dagger.Component;
 @Singleton  // Constraints this component to one-per-application or un-scoped bindings.
 @Component(modules = ApplicationModule.class)
 public interface ApplicationComponent {
-    // Exposed to sub-graphs.
+    /**
+     * This is actually an important property of how components work in Dagger:
+     * they do not expose types from their modules unless you explicitly make them available.
+     */
     PlaygroundApplication application();
 }
