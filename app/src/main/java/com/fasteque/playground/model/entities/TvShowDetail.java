67436f0f39c6377d4bc0d57694c59f5f686d1b9b
fasteque <info@fasteque.com>
2015-05-25 17:47:55 +0200

    Model class placeholder

---
+++ app/src/main/java/com/fasteque/playground/model/entities/TvShowDetail.java
@@ -0,0 +1,26 @@
+package com.fasteque.playground.model.entities;
+
+/**
+ * Created by danielealtomare on 25/05/15.
+ * Project: Playground
+ */
+public class TvShowDetail {
+    /*
+    {"backdrop_path":null,"created_by":[],"episode_run_time":[],
+    "first_air_date":"2015-05-25",
+    "genres":[],
+    "homepage":"http://www.foxplaybrasil.com.br/show/12003-lucky-ladies",
+    "id":62723,"in_production":true,
+    "languages":["pt"],"last_air_date":"2015-05-25","name":"Lucky Ladies",
+    "networks":[{"id":1117,"name":"Fox Life (Brasil)"}],
+    "number_of_episodes":null,"number_of_seasons":1,
+    "origin_country":["BR"],"original_language":"pt",
+    "original_name":"Lucky Ladies","overview":"",
+    "popularity":4.21875,"poster_path":"/1hNGcMomOJMAt2223LOACaFj0AB.jpg",
+    "production_companies":[],
+    "seasons":[{"air_date":"2015-05-25","episode_count":1,"id":66905,"poster_path":null,"season_number":1}],
+    "status":"Returning Series","type":"Reality","vote_average":0.0,"vote_count":0}
+    */
+
+    // TODO
+}
