398b85f9a1c56c538cb3fb38db96fb6373823d6c
fasteque <info@fasteque.com>
2015-05-26 21:30:18 +0200

    Defined rest service interface

---
+++ app/src/main/java/com/fasteque/playground/model/rest/MovieDbApi.java
@@ -0,0 +1,26 @@
+package com.fasteque.playground.model.rest;
+
+import com.fasteque.playground.model.entities.TvShowDetail;
+import com.fasteque.playground.model.entities.TvShowsWrapper;
+
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
+import rx.Observable;
+
+/**
+ * Created by danielealtomare on 26/05/15.
+ * Project: Playground
+ */
+public interface MovieDbApi {
+    String END_POINT = "https://api.themoviedb.org/3/";
+    String PARAM_API_KEY = "api_key";
+
+    @GET("tv/airing_today")
+    Observable<TvShowsWrapper>
+    getTvShowsAiringToday(@Query("page") int page);
+
+    @GET("tv/")
+    Observable<TvShowDetail>
+    getTvShowDetail(@Path("id") String id);
+}
