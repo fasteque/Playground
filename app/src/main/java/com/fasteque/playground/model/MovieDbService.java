1f2e4a9075e008ffb3579c5b65ccddd592d3ec7c
fasteque <info@fasteque.com>
2015-05-26 22:31:07 +0200

    Defined rest service interface

---
+++ app/src/main/java/com/fasteque/playground/model/MovieDbService.java
@@ -0,0 +1,15 @@
+package com.fasteque.playground.model;
+
+import com.fasteque.playground.model.entities.TvShowDetail;
+import com.fasteque.playground.model.entities.TvShowsWrapper;
+
+import rx.Observable;
+
+/**
+ * Created by danielealtomare on 26/05/15.
+ * Project: Playground
+ */
+public interface MovieDbService {
+    Observable<TvShowsWrapper> getTvShowsAiringToday(final int page);
+    Observable<TvShowDetail> getTvShowDetail(final String id);
+}
