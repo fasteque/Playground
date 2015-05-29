ba986a30be60fabb4e2aec7bf0f982b7f66e71ce
fasteque <info@fasteque.com>
2015-05-29 21:16:41 +0200

    Set Toolbar as ActionBar

--- app/src/main/java/com/fasteque/playground/views/activities/TvShowDetailActivity.java
+++ app/src/main/java/com/fasteque/playground/views/activities/TvShowDetailActivity.java
@@ -2,17 +2,27 @@ package com.fasteque.playground.views.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.fasteque.playground.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class TvShowDetailActivity extends AppCompatActivity {
+
+    @InjectView(R.id.show_detail_toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_show_detail);
+
+        ButterKnife.inject(this);
+        setSupportActionBar(toolbar);
     }
 
     @Override
