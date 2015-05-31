71b11bbac02d7914d8f7e0a9be89f3d6c0e8146a
fasteque <info@fasteque.com>
2015-05-31 11:34:24 +0200

    Added use cases to presenters

--- app/src/main/java/com/fasteque/playground/presenters/TvShowDetailPresenter.java
+++ app/src/main/java/com/fasteque/playground/presenters/TvShowDetailPresenter.java
@@ -1,12 +1,25 @@
 package com.fasteque.playground.presenters;
 
+import com.fasteque.playground.domain.GetTvShowDetailUseCase;
+import com.fasteque.playground.views.TvShowDetailView;
 import com.fasteque.playground.views.View;
 
 /**
  * Created by danielealtomare on 25/05/15.
  * Project: Playground
  */
+// TODO: extends Subsciber
 public class TvShowDetailPresenter implements Presenter {
+
+    private TvShowDetailView tvShowDetailView;
+    private final GetTvShowDetailUseCase getTvShowDetailUseCase;
+    private String tvShowId;
+
+    // FIXME: add injection.
+    public TvShowDetailPresenter(GetTvShowDetailUseCase getTvShowDetailUseCase) {
+        this.getTvShowDetailUseCase = getTvShowDetailUseCase;
+    }
+
     @Override
     public void onStart() {
 
--- app/src/main/java/com/fasteque/playground/presenters/TvShowsPresenter.java
+++ app/src/main/java/com/fasteque/playground/presenters/TvShowsPresenter.java
@@ -1,12 +1,24 @@
 package com.fasteque.playground.presenters;
 
+import com.fasteque.playground.domain.GetAiringTodayUseCase;
+import com.fasteque.playground.views.TvShowsView;
 import com.fasteque.playground.views.View;
 
 /**
  * Created by danielealtomare on 25/05/15.
  * Project: Playground
  */
+// TODO: extends Subsciber
 public class TvShowsPresenter implements Presenter {
+
+    private TvShowsView tvShowsView;
+    private final GetAiringTodayUseCase getAiringTodayUseCase;
+
+    //FIXME: add injection
+    public TvShowsPresenter(GetAiringTodayUseCase getAiringTodayUseCase) {
+        this.getAiringTodayUseCase = getAiringTodayUseCase;
+    }
+
     @Override
     public void onStart() {
 
