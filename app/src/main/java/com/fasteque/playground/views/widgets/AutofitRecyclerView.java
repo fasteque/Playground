ed325ed4f19cf71b2f9bf638fe10a902a96c95e0
fasteque <info@fasteque.com>
2015-06-04 21:47:42 +0200

    Implemented custom RecyclerView

---
+++ app/src/main/java/com/fasteque/playground/views/widgets/AutofitRecyclerView.java
@@ -0,0 +1,64 @@
+package com.fasteque.playground.views.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by danielealtomare on 04/06/15.
+ * Project: Playground
+ */
+public class AutofitRecyclerView extends RecyclerView {
+
+    private GridLayoutManager layoutManager;
+    private int columnWidth = -1;
+
+    public AutofitRecyclerView(Context context) {
+
+        super(context);
+        init(context, null);
+    }
+
+    public AutofitRecyclerView(Context context, AttributeSet attrs) {
+
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public AutofitRecyclerView(Context context, AttributeSet attrs, int defStyle) {
+
+        super(context, attrs, defStyle);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+        if (attrs != null) {
+
+            int[] attrsArray = {
+                    android.R.attr.columnWidth
+            };
+
+            TypedArray array = context.obtainStyledAttributes(attrs, attrsArray);
+            columnWidth = array.getDimensionPixelSize(0, -1);
+            array.recycle();
+        }
+
+        layoutManager = new GridLayoutManager(getContext(), 2);
+        setLayoutManager(layoutManager);
+    }
+
+    @Override
+    protected void onMeasure(int widthSpec, int heightSpec) {
+
+        super.onMeasure(widthSpec, heightSpec);
+
+        if (columnWidth > 0) {
+
+            int spanCount = Math.max(1, getMeasuredWidth() / columnWidth);
+            layoutManager.setSpanCount(spanCount);
+        }
+    }
+}
