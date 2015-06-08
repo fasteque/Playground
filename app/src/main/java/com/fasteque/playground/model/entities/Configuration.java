0273e2332c339479d3ee3a760bec6994acd1a742
fasteque <info@fasteque.com>
2015-06-08 22:21:07 +0200

    Improved configuration class definition

--- app/src/main/java/com/fasteque/playground/model/entities/Configuration.java
+++ app/src/main/java/com/fasteque/playground/model/entities/Configuration.java
@@ -17,6 +17,11 @@ public class Configuration {
     public class ConfigurationImages {
         private String base_url;
         private String secure_base_url;
+        private String[] backdrop_sizes;
+        private String[] logo_sizes;
+        private String[] poster_sizes;
+        private String[] profile_sizes;
+        private String[] still_sizes;
 
         public String getBase_url() {
             return base_url;
@@ -24,6 +29,26 @@ public class Configuration {
 
         public String getSecure_base_url() {
             return secure_base_url;
+        }
+
+        public String[] getBackdrop_sizes() {
+            return backdrop_sizes;
+        }
+
+        public String[] getLogo_sizes() {
+            return logo_sizes;
+        }
+
+        public String[] getPoster_sizes() {
+            return poster_sizes;
+        }
+
+        public String[] getProfile_sizes() {
+            return profile_sizes;
+        }
+
+        public String[] getStill_sizes() {
+            return still_sizes;
         }
     }
 }
