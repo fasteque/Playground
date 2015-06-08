56e4610c9308fc735d8d423acff7281e6eb4bf86
fasteque <info@fasteque.com>
2015-06-08 22:21:33 +0200

    Keep track of path tokens used by the rest service

---
+++ app/src/main/java/com/fasteque/playground/utils/MovieDbConstants.java
@@ -0,0 +1,31 @@
+package com.fasteque.playground.utils;
+
+/**
+ * Created by danielealtomare on 08/06/15.
+ * Project: Playground
+ */
+public final class MovieDbConstants {
+
+    private MovieDbConstants() {
+
+    }
+
+    private static String BASIC_STATIC_URL = "";
+    private static String POSTER_PREFERRED_SIZE = "";
+
+    public static String getBasicStaticUrl() {
+        return BASIC_STATIC_URL;
+    }
+
+    public static void setBasicStaticUrl(String basicStaticUrl) {
+        BASIC_STATIC_URL = basicStaticUrl;
+    }
+
+    public static String getPosterPreferredSize() {
+        return POSTER_PREFERRED_SIZE;
+    }
+
+    public static void setPosterPreferredSize(String posterPreferredSize) {
+        POSTER_PREFERRED_SIZE = posterPreferredSize;
+    }
+}
