ec43889b4af5d0068b433a6e4e8640e7faecfa92
fasteque <info@fasteque.com>
2015-06-12 22:34:41 +0200

    Defined new Presenter API

--- app/src/main/java/com/fasteque/playground/presenters/Presenter.java
+++ app/src/main/java/com/fasteque/playground/presenters/Presenter.java
@@ -1,5 +1,7 @@
 package com.fasteque.playground.presenters;
 
+import android.content.Intent;
+
 import com.fasteque.playground.views.View;
 
 /**
@@ -10,4 +12,5 @@ public interface Presenter {
     void onPresenterStart();
     void onPresenterStop();
     void attachView(View view);
+    void attachIncomingIntent(Intent intent);
 }
