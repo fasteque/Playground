5b76527cf9682f0390fb63be9be4d217d84d7105
fasteque <info@fasteque.com>
2015-06-13 19:04:24 +0200

    New interface method for the details view

--- app/src/main/java/com/fasteque/playground/views/TvShowDetailView.java
+++ app/src/main/java/com/fasteque/playground/views/TvShowDetailView.java
@@ -1,9 +1,11 @@
 package com.fasteque.playground.views;
 
+import com.fasteque.playground.model.entities.TvShowDetail;
+
 /**
  * Created by danielealtomare on 25/05/15.
  * Project: Playground
  */
 public interface TvShowDetailView extends View {
-    // TODO
+    void displayTvShowDetail(TvShowDetail tvShowDetail);
 }
