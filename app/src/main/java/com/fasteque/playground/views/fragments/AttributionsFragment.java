a44d0e2dfaf1098dd3a075e3e32c4ceab8a34c25
fasteque <daniele.altomare@gmail.com>
2015-11-30 15:07:26 +0100

    reduced class member visibility

--- app/src/main/java/com/fasteque/playground/views/fragments/AttributionsFragment.java
+++ app/src/main/java/com/fasteque/playground/views/fragments/AttributionsFragment.java
@@ -15,7 +15,7 @@ import com.fasteque.playground.R;
  */
 public class AttributionsFragment extends PreferenceFragment {
 
-    OnPreferenceSelectedListener onPreferenceSelectedListener;
+    private OnPreferenceSelectedListener onPreferenceSelectedListener;
 
     public interface OnPreferenceSelectedListener {
         void onPreferenceWithUriSelected(Uri uri);
